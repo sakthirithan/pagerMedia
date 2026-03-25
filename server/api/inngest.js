b2e21376f517537d7a3ee56df699d93a34a60bc9
sakthirithan <sakthirithanofficial@gmail.com>
2026-03-25 13:49:49 +0530

    FIX: Inngest.js Files modifies with exact codes

--- server/api/inngest.js
+++ server/api/inngest.js
@@ -1,8 +1,27 @@
 import { serve } from "inngest/express";
+import connectDB from "../configs/db.js";
+import { inngest, functions } from "../inngest/index.js";
 
-export default function handler(req, res) {
-  return serve({
-    client: { id: "test" }, // dummy test
-    functions: [],
-  })(req, res);
+let isConnected = false;
+
+const connectDatabase = async () => {
+  if (!isConnected) {
+    await connectDB();
+    isConnected = true;
+  }
+};
+
+export default async function handler(req, res) {
+  try {
+    await connectDatabase();
+
+    return serve({
+      client: inngest,
+      functions,
+    })(req, res);
+
+  } catch (error) {
+    console.error("Inngest Error:", error.message);
+    res.status(500).send("Internal Server Error");
+  }
 }
