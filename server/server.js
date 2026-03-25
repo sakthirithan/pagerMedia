b9e2408cbdd69b7de94e284c1b39eaf472874aa8
sakthirithan <sakthirithanofficial@gmail.com>
2026-03-25 11:43:36 +0530

    FIX: Server issues

--- server/server.js
+++ server/server.js
@@ -1,31 +1,52 @@
+// import express from "express";
+// import cors from "cors";
+// import "dotenv/config";
+// import connectDB from "./configs/db.js";
+// import { inngest, functions } from "./inngest/index.js";
+// import { serve } from "inngest/express";
+
+// const app = express();
+// const PORT = 4000
+// // Middleware
+// app.use(express.json());
+// app.use(cors());
+
+// // Connect to DB
+//   try {
+//     await connectDB()
+
+//   } catch (error) {
+//     console.error("DB connection failed:", error.message);
+//   }
+
+// // Routes
+// app.get("/", (req, res) => res.send("Server is Running Successfully"));
+// app.use("/api/inngest", serve({ client: inngest, functions }));
+// // Start the server
+// app.listen(PORT, () => {
+//   console.log(`Server is running at http://localhost:${PORT}`);
+// });
+
+
+// export default app;
+
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from "./configs/db.js";
-import { inngest, functions } from "./inngest/index.js";
-import { serve } from "inngest/express";
 
 const app = express();
-const PORT = 4000
-// Middleware
+
 app.use(express.json());
 app.use(cors());
 
-// Connect to DB
+app.get("/", async (req, res) => {
   try {
-    await connectDB()
-
+    await connectDB();
+    res.send("Server is Running ✅");
   } catch (error) {
-    console.error("DB connection failed:", error.message);
+    res.status(500).send("DB Error: " + error.message);
   }
-
-// Routes
-app.get("/", (req, res) => res.send("Server is Running Successfully"));
-app.use("/api/inngest", serve({ client: inngest, functions }));
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
 });
-
 
 export default app;
