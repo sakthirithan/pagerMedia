8d91fcc2726b8bd4c80a4f2b2efab500a200c7c7
sakthirithan <sakthirithanofficial@gmail.com>
2026-03-25 15:11:06 +0530

    FIX: Inngest Function errors

--- server/server.js
+++ server/server.js
@@ -2,21 +2,25 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from "./configs/db.js";
-import { inngest, functions } from './inngest/index.js'
-import { serve } from 'inngest/express'
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-await connectDB();
+app.get("/", async (req, res) => {
+  try {
+    await connectDB();
+    res.send("server is running");
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
 
-app.get('/', (req, res)=> res.send('server is running'))
-app.use("/api/inngest", serve({client: inngest, functions }));
+const PORT = process.env.PORT || 4000;
 
-const PORT = process.env.PORT || 4000
-
-app.listen(PORT, ()=> console.log(`server is running on port ${PORT}`))
+app.listen(PORT, () =>
+  console.log(`server is running on port ${PORT}`)
+);
 
 export default app;
